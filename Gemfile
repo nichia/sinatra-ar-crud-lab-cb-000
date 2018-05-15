8beed685090665cbf4214b6aad6c3b0dcf2153b7
nichia <nichia@gmail.com>
2018-05-15 14:00:08 -0400

    Done.

--- Gemfile
+++ Gemfile
@@ -4,9 +4,9 @@ gem 'activerecord', :require => 'active_record'
 gem 'sinatra-activerecord', :require => 'sinatra/activerecord'
 
 gem 'sinatra'
-gem 'sqlite3'
-gem 'tux'
-gem 'pry'
+#gem 'sqlite3'
+#gem 'tux'
+#gem 'pry'
 gem 'pry-nav'
 gem 'rake'
 gem 'rspec'
@@ -16,7 +16,7 @@ gem 'require_all'
 
 
 group :development do
-  gem "capybara"
+#  gem "capybara"
   gem "sqlite3"
   gem "pry"
   gem "shotgun"
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,54 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  # Create get: Load new form for user to input new blog post
+  get '/posts/new' do
+    erb :new
+  end
 
+  # Read get: Load index page to list all blog posts
+  get '/posts' do
+    @posts = Post.all
+
+    erb :index
+  end
+
+  # Create post: Create new post to the database
+  post '/posts' do
+    @post = Post.create(params)
+
+    redirect :posts
+  end
+
+  # Read get: Load show page to display an individual blog post
+  get '/posts/:id' do
+    @post = Post.find_by_id(params[:id])
+
+    erb :show
+  end
+
+ # Update get:  Load edit page to display an individual blog post and allow user to edit the post instance
+  get '/posts/:id/edit' do
+    @post = Post.find_by_id(params[:id])
+
+    erb :edit
+  end
+
+  # Edit Patch post: Update the post instance
+  patch '/posts/:id' do
+    post = Post.find_by_id(params[:id])
+    post.name = params[:name]
+    post.content = params[:content]
+    post.save
+
+    redirect :"/posts/#{post.id}"
+  end
+
+  # Delete Post: Delete the post instance
+  delete '/posts/:id/delete' do
+    @post = Post.find_by_id(params[:id])
+    @post.destroy
+
+    erb :delete
   end
 end
